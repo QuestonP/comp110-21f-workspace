3b1a92bef116d86e9235fe2d5bc138da83c48469
Queston Parker <questonp123@gmail.com>
2021-10-26 23:33:33 -0400

    data utils

---
+++ lessons/data_utils.py
@@ -0,0 +1,36 @@
+"""Some helpful utility functions for CSV Files."""
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a CSV into a 'table'"""
+    result: list[dict[str, str]] = []
+    # read file
+    file_handle = open(filename, "r", encoding="utf8")
+
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+
+    # close file 
+    file_handle.close()
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column"""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform row oriented table to column oriented one."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
